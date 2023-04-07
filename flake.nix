052c612e22cf76a59ac2577cfdf99f82560bfe9d
nokogiri <nokogiri@gefjon.org>
2023-04-07 22:16:15 +0200

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, fenix, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      packages.default =
+        let
+          toolchain = fenix.packages.i686-linux.stable.toolchain;
+          pkgs = nixpkgs.legacyPackages.i686-linux;
+        in
+
+        (pkgs.makeRustPlatform {
+          cargo = toolchain;
+          rustc = toolchain;
+        }).buildRustPackage {
+          pname = "extest";
+          version = "0.1.0";
+
+          src = ./.;
+
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+    });
+}
